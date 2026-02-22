10faea574f82cd7111316c91c7c3d9b1f962bf9f
lekan <oyeniranolalekan89@gmail.com>
2026-02-22 18:20:47 +0100

    first commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const profile = () => {
+  return (
+    <SafeAreaView>
+      <Text className="font-bold text-light-100">profile</Text>
+    </SafeAreaView>
+  )
+}
+
+export default profile
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary : "#151313",
+        light: {
+          100: "#d5c6ff",
+          200: "#a8b5db",
+          300: "#9ca4ab",
+        },
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
+          300: "#4d2f7f",
+        },
+        accent : "#ab8bff",
+      },
+    },
+  },
+  plugins: [],
+}
+
